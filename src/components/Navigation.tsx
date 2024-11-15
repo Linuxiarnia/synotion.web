a50030e99f5302f8fb9d42fe014c0582fc44abb3
Prx1i1 <pawlo.augustyn@gmail.com>
2024-11-15 20:56:03 +0100

    fix activeButton

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,33 +1,36 @@
 import { Box, Button, ButtonProps, Theme, Typography, useTheme } from '@mui/material';
 import React from 'react';
-// import { useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 import './Navigation.css';
 import styled from '@emotion/styled';
 
 //myButton zwraca blad, ale dziala lol
-const MyButton = styled(Button)<ButtonProps>(({ theme }) => ({ color: (theme as Theme).palette.text.primary }));
+const MyButton = styled(Button)<ButtonProps>(({ theme, href }) => ((href==location.pathname)? { color: (theme as Theme).palette.primary.main } : { color: (theme as Theme).palette.text.primary }));
 
 
 export const NavBar = () => {
     // type Page = 'Home' | 'Leaderboards' | 'Profile' | 'Settings';
     const theme = useTheme();
-    //const location = useLocation();
-   
+    
+    const location = useLocation();
+
+    console.log(location.pathname);
+
     return(
         <div className='navigation'>
 
             <span>
                 <Box sx={{ bgcolor: 'none' , m: 2 }}>
-                    <Typography color='text.primary' variant='body1'>
+                    <Typography color={theme.palette.primary.main} variant='body1'>
                         placeholderLogo
                     </Typography>
                 </Box>  
                 <div className='navContainer'>
-                    <Box><MyButton href = '/'>Icon: Play</MyButton></Box>
+                    <Box><MyButton href='/'>Icon: Play</MyButton></Box>
                     <Box><MyButton href = '/leaderboards'>Leaderboards</MyButton></Box>
-                    <Box><MyButton href = '/profile' sx={{ color: theme.palette.text.primary }}>Profile</MyButton></Box>
-                    <Box><MyButton href = '/settings' sx={{ color: theme.palette.text.primary }}>Settings</MyButton></Box>
+                    <Box><MyButton href = '/profile'>Profile</MyButton></Box>
+                    <Box><MyButton href = '/settings'>Settings</MyButton></Box>
                 </div>
             </span>
             <div className='logoutContainer'>
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+
 const router = createBrowserRouter([
     {
         path: '/',
         element: <App/>
     },
+    {
+        path: '/leaderboards',
+        element: <App/>
+    }
 ]);
 
 createRoot(document.getElementById('root')!).render(
