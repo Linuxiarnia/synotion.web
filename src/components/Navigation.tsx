02d5088b28740d9b522c3ab16c7ccc8dc2fa4372
Prx1i1 <pawlo.augustyn@gmail.com>
2024-11-15 18:38:19 +0100

    test all

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+export const NavBar = () => {
+
+    type Page = 'Home' | ""
+
+    const [currentPage: Page, setCurrentPage] = useState('Home');
+
+    return(<>
+    
+    </>);
+};
