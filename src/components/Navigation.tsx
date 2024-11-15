e522c7406fa33cd3ff5730907c946a5f4340a4e2
Prx1i1 <pawlo.augustyn@gmail.com>
2024-11-15 20:24:25 +0100

    fix newButtons nav

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,31 +1,37 @@
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, ButtonProps, Typography, useTheme } from '@mui/material';
 import React from 'react';
+// import { useLocation } from 'react-router-dom';
 
 import './Navigation.css';
+import styled from '@emotion/styled';
+
+//myButton zwraca blad, ale dziala lol
+const MyButton = styled(Button)<ButtonProps>(({ theme }) => ({ color: theme.palette.text.primary }));
 
 
 export const NavBar = () => {
-
     // type Page = 'Home' | 'Leaderboards' | 'Profile' | 'Settings';
-
+    const theme = useTheme();
+    //const location = useLocation();
+   
     return(
         <div className='navigation'>
 
             <span>
                 <Box sx={{ bgcolor: 'none' , m: 2 }}>
-                    <Typography color='primary' variant='body1'>
+                    <Typography color='text.primary' variant='body1'>
                         placeholderLogo
                     </Typography>
                 </Box>  
                 <div className='navContainer'>
-                    <Box><Button href = '/'>Icon: Play</Button></Box>
-                    <Box><Button href = '/leaderboards'>Leaderboards</Button></Box>
-                    <Box><Button href = '/profile'>Profile</Button></Box>
-                    <Box><Button href = '/settings'>Settings</Button></Box>
+                    <Box><MyButton href = '/'>Icon: Play</MyButton></Box>
+                    <Box><MyButton href = '/leaderboards'>Leaderboards</MyButton></Box>
+                    <Box><MyButton href = '/profile' sx={{ color: theme.palette.text.primary }}>Profile</MyButton></Box>
+                    <Box><MyButton href = '/settings' sx={{ color: theme.palette.text.primary }}>Settings</MyButton></Box>
                 </div>
             </span>
             <div className='logoutContainer'>
-                <div><Button onClick={() => {/* logout */}}>Icon: Log out</Button></div>
+                <div><MyButton onClick={() => {/* logout */}}>Icon: Log out</MyButton></div>
             </div>
         </div>
     );
