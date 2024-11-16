15758e7792106f00f1833b729278e6f08122bc78
Oliwier Matus <matusoliwier@gmail.com>
2024-11-16 12:46:24 +0100

    feture: register

---
+++ src/views/register/Register.tsx
@@ -0,0 +1,46 @@
+import { Button, Card, Stack, TextField, useTheme } from '@mui/material';
+import React, { useState } from 'react';
+import { useProvider } from '@providers/ThemeContext';
+import { Email } from '@mui/icons-material';
+
+export const Login = () => {
+
+    const theme = useTheme();
+    const { jwtoken, overWriteToken } = useProvider();
+    
+    const [login, setLogin] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+
+    const sendRegister = (login: string,email:string, password: string) => {
+        const result: string = login + email + password; //temp;
+        //call the api
+
+
+        overWriteToken(result);
+    };
+
+    return(
+        <Card sx={{ margin: '10px', paddingY: '10px' }}>
+            <Stack useFlexGap spacing = {3} sx={{
+                margin: '10px', marginLeft: 'auto', marginRight: 'auto' 
+            }}>
+                <TextField required id='email' label='Email' type='email' sx={{
+                    width: '90%', padding: '10px', marginLeft: 'auto', marginRight: 'auto' 
+                }}
+                onChange={(e) => setEmail(e.target.value)}></TextField> 
+                <TextField required id='login' label='Login' sx={{
+                    width: '90%', padding: '10px', marginLeft: 'auto', marginRight: 'auto' 
+                }}
+                onChange={(e) => setLogin(e.target.value)}></TextField>
+                <TextField required id='password' label='Password' type='password' sx={{
+                    width: '90%', padding: '10px', marginLeft: 'auto', marginRight: 'auto' 
+                }}
+                onChange={(e) => setPassword(e.target.value)}></TextField>
+                <Button sx={{
+                    width: '90%', padding: '10px', marginLeft: 'auto', marginRight: 'auto', border: '1px solid', borderColor: theme.palette.primary.main
+                }}
+                onClick={() => sendRegister(login,email, password)}>Register</Button>
+            </Stack>
+        </Card>);
+};
