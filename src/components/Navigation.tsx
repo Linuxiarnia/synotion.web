acd213bfaa1c6b1f0e853d7b786a2ad3c7bf15e2
Prx1i1 <pawlo.augustyn@gmail.com>
2024-11-16 06:20:10 +0100

    fix nav??

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,8 +1,9 @@
-import { Box, Button, ButtonProps, Stack, Theme, Typography, useTheme } from '@mui/material';
+import { Box, Button, ButtonProps, Grid2, Stack, Theme, Typography, useTheme } from '@mui/material';
 import React from 'react';
 
 
 import styled from '@emotion/styled';
+import { Margin } from '@mui/icons-material';
 
 //myButton zwraca blad, ale dziala lol
 const MyButton = styled(Button)<ButtonProps>(({ theme, href }) => ((href==window.location.pathname)? { color: (theme as Theme).palette.primary.main } : { color: (theme as Theme).palette.text.primary }));
@@ -15,27 +16,23 @@ export const NavBar = () => {
     console.log(location);
 
     return(
-        <Stack sx={{
+        <Grid2 container alignContent='center' spacing={70} sx={{
             
             borderRightColor: theme.palette.primary.main, borderWidth: '3px', borderRightStyle: 'solid' , height: '100%'
         }}>
 
-            <span>
-                <Box sx={{ bgcolor: 'none' , m: 2 }}>
-                    <Typography color={theme.palette.primary.main} variant='body1'>
-                        placeholderLogo
-                    </Typography>
-                </Box>  
-                <div className='navContainer'>
-                    <Box><MyButton href='/game'>Icon: Play</MyButton></Box>
-                    <Box><MyButton href = '/leaderboards'>Leaderboards</MyButton></Box>
-                    <Box><MyButton href = '/profile'>Profile</MyButton></Box>
-                    <Box><MyButton href = '/settings'>Settings</MyButton></Box>
-                </div>
-            </span>
-            <div className='logoutContainer'>
-                <div><MyButton onClick={() => {/* logout */}}>Icon: Log out</MyButton></div>
-            </div>
-        </Stack>
+            <Grid2 size = {11} sx={{ width:'80%', margin: 'auto' }}>
+                <Box sx={{ paddingY: '10px', marginBottom: '70px' }}>
+                    placeholderLogo
+                </Box>
+                <Box><MyButton href='/game'>Icon: Play</MyButton></Box>
+                <Box><MyButton href = '/leaderboards'>Leaderboards</MyButton></Box>
+                <Box><MyButton href = '/profile'>Profile</MyButton></Box>
+                <Box><MyButton href = '/settings'>Settings</MyButton></Box>
+            </Grid2>
+            <Grid2 size = {1} sx = {{ width: '80%', margin: 'auto' }}>
+                <MyButton onClick={() => {/* logout */}}>Icon: Log out</MyButton>
+            </Grid2>
+        </Grid2>
     );
 };
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -4,11 +4,11 @@ import React, { PropsWithChildren } from 'react';
 
 export const Layout: React.FC<PropsWithChildren> = ({ children }) => {
     return<>
-        <Grid2 container columns={16} spacing={2}>
-            <Grid2 size={3} sx={{ display: 'block' }}>
+        <Grid2 container columns={13} spacing={2}>
+            <Grid2 size={2} sx={{ display: 'block' }}>
                 <NavBar />
             </Grid2>
-            <Grid2 size={13} sx={{ display: 'block' }}>
+            <Grid2 size={11} sx={{ display: 'block' }}>
                 <Box>
                     {children}
                 </Box>
