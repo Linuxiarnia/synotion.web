aff2545aaef3dc84f8c9f69ad60a45266ee3fead
Prx1i1 <pawlo.augustyn@gmail.com>
2024-11-16 06:59:30 +0100

    meow

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,9 +1,9 @@
-import { Box, Button, ButtonProps, Grid2, Stack, Theme, Typography, useTheme } from '@mui/material';
+import { Box, Button, ButtonProps, Grid2, Stack, Theme, useTheme } from '@mui/material';
 import React from 'react';
 
 
 import styled from '@emotion/styled';
-import { Margin } from '@mui/icons-material';
+import { Leaderboard, Person, PlayArrow, Settings } from '@mui/icons-material';
 
 //myButton zwraca blad, ale dziala lol
 const MyButton = styled(Button)<ButtonProps>(({ theme, href }) => ((href==window.location.pathname)? { color: (theme as Theme).palette.primary.main } : { color: (theme as Theme).palette.text.primary }));
@@ -16,21 +16,26 @@ export const NavBar = () => {
     console.log(location);
 
     return(
-        <Grid2 container alignContent='center' spacing={70} sx={{
-            
+        <Grid2 container sx={{
             borderRightColor: theme.palette.primary.main, borderWidth: '3px', borderRightStyle: 'solid' , height: '100%'
         }}>
 
-            <Grid2 size = {11} sx={{ width:'80%', margin: 'auto' }}>
-                <Box sx={{ paddingY: '10px', marginBottom: '70px' }}>
-                    placeholderLogo
+            <Grid2 size = {2} sx={{ width:'80%', margin: 'auto' }}>
+                <Box sx={{ color: theme.palette.primary.main }}>
+                    Synoption
                 </Box>
-                <Box><MyButton href='/game'>Icon: Play</MyButton></Box>
-                <Box><MyButton href = '/leaderboards'>Leaderboards</MyButton></Box>
-                <Box><MyButton href = '/profile'>Profile</MyButton></Box>
-                <Box><MyButton href = '/settings'>Settings</MyButton></Box>
             </Grid2>
-            <Grid2 size = {1} sx = {{ width: '80%', margin: 'auto' }}>
+            <Grid2 size = {9} sx={{
+                width: '80%', margin: 'auto', marginTop: '0px' 
+            }}>
+                <Stack direction='row' spacing={2}><PlayArrow></PlayArrow><MyButton href='/game'>Play</MyButton></Stack>
+                <Stack direction='row' spacing={2}><Leaderboard></Leaderboard><MyButton href = '/leaderboards'>Leaderboards</MyButton></Stack>
+                <Stack direction='row' spacing={2}><Person></Person><MyButton href = '/profile'>Profile</MyButton></Stack>
+                <Stack direction='row' spacing={2}><Settings></Settings><MyButton href = '/settings'>Settings</MyButton></Stack>
+            </Grid2>
+            <Grid2 size = {1} sx = {{
+                width: '80%', margin: 'auto', marginBottom: '10px' 
+            }}>
                 <MyButton onClick={() => {/* logout */}}>Icon: Log out</MyButton>
             </Grid2>
         </Grid2>
--- src/components/summary.tsx
+++ src/components/summary.tsx
@@ -1,4 +1,4 @@
-import { Box, Card } from '@mui/material';
+import { Card } from '@mui/material';
 import React from 'react';
 
 export interface summaryElemnts {
